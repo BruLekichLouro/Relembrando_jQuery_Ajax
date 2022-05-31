4503a5daafa4d332cb4ed850753047b5d118d9dc
BruLekichLouro <bububio@yahoo.com.br>
2022-05-31 11:12:17 -0300

    Adicionando parágrafos para mostrar as informações do CEP

--- js/main.js
+++ js/main.js
@@ -7,6 +7,11 @@ function consultaCep(){
         type:"GET",
         success: function(response) {
             console.log(response.logradouro);
+            //document.getElementById("logradouro").innerHTML = response.logradouro;
+            $("#logradouro").html(response.logradouro);//utilizando jQuery: pego o id
+            // e passo para o html o que eu quero que ele mostre
+            document.getElementById("uf").innerHTML = response.uf;
+            document.getElementById("localidade").innerHTML = response.localidade;
         }
     });
 }
